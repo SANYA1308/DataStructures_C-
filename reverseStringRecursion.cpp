66d2dd7e2a8081e67a89a17f23ae86b010ac581e
Sanya <sanyagondi1308@gmail.com>
2023-01-04 21:52:31 +0530

    ReverseStringRecursion

---
+++ reverseStringRecursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void reverseString(string& str, int s, int e){
+    if(s>e){
+        return ;
+
+    }
+    swap(str[s], str[e]);
+    s++;
+    e--;
+    reverseString(str,s,e);
+}
+
+
+int main(){
+    string name = "sanya";
+    reverseString(name, 0, name.length()-1);
+    cout<<name<<endl;
+}
